01841744d7fede6fa5c046cd3d445e0aef58a9da
MateuszPuszcz <mateuszpuszcz.official@gmail.com>
2022-03-19 14:49:39 +0100

    zadanie

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let dataUrl = 'https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php'
+
+$('#btn-load').on('click', function()
+{
+  if($('#dane-programisty').length===0)
+  {
+  $("<div id='dane-programisty'>").appendTo('body') 
+  }
+  
+
+$.getJSON(dataUrl, function(dane)
+{
+$('<p>').text(dane.imie).appendTo('#dane-programisty');
+$('<p>').text(dane.nazwisko).appendTo('#dane-programisty');
+$('<p>').text(dane.zawod).appendTo('#dane-programisty');
+$('<p>').text(dane.firma).appendTo('#dane-programisty');
+});
+})
